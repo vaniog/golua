44a3a2656fdd055a7aab920cb46f7d5c8fcf14cc
vaniog <vaniog@yandex-team.ru>
2025-11-15 23:27:10 +0300

    make possible to wasm compile

--- lib/oslib/clock.go
+++ lib/oslib/clock.go
@@ -4,14 +4,16 @@
 package oslib
 
 import (
-	"syscall"
+	// "syscall"
+
+	"time"
 
 	rt "github.com/arnodel/golua/runtime"
 )
 
 func clock(t *rt.Thread, c *rt.GoCont) (rt.Cont, error) {
-	var rusage syscall.Rusage
-	_ = syscall.Getrusage(syscall.RUSAGE_SELF, &rusage) // ignore errors
-	time := float64(rusage.Utime.Sec+rusage.Stime.Sec) + float64(rusage.Utime.Usec+rusage.Stime.Usec)/1000000.0
-	return c.PushingNext1(t.Runtime, rt.FloatValue(time)), nil
+	// var rusage syscall.Rusage
+	// _ = syscall.Getrusage(syscall.RUSAGE_SELF, &rusage) // ignore errors
+	// time := float64(rusage.Utime.Sec+rusage.Stime.Sec) + float64(rusage.Utime.Usec+rusage.Stime.Usec)/1000000.0
+	return c.PushingNext1(t.Runtime, rt.FloatValue(float64(time.Now().UnixNano())/1000000.)), nil
 }
